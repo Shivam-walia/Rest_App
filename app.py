d0876736bcaa1d5145dbee4a8faca568150ace0d
Shivam-walia <shivamw65@gmail.com>
2021-02-10 11:51:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ api=Api(MyApp)
 @api.route('/Helloworld')
 class Helloworld(Resource):
     def get(self):
+        print("Welcome to Flask  Application")
         return ("Data Hello World")
            
 if __name__=='__main__':
