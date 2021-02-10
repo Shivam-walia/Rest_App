017e864d28575c4e772e3eb52e2f5c67619e97c4
Shivam-walia <shivamw65@gmail.com>
2021-02-10 11:32:07 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask_restplus import Api,Resource,
+from flask import Flask
+
+
+#%%
+
+MyApp=Flask(__name__)
+
+api=Api(MyApp)
+
+
+@api.route('/Helloworld')
+class Helloworld(Resource):
+    def get(self):
+        return {'Data': 'Hello World'}
+            
+        
+
+#%%  
+if __name__=='__main__':
+    MyApp.run(debug=True)
+        
+        
+        
